eefaf79f306dbd62ac8a00bbcecd7af54b435a23
vivianebresolin <vivianebresolin@gmail.com>
2021-04-22 13:45:21 -0300

    SPA x SSR x SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,60 @@
-export default function Home() {
+//SSG
+export default function Home(props) {
   return (
-    <h1>index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
 }
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, //revalidade - tempo em segundos, serve para dizer a cada quanto tempo se quer que seja gerada uma nova página estática (gera uma nova chamada para a API).
+  }                          //nesse exemplo, seria gerada a cada 8 horas  
+}
+
+//SSR
+// export default function Home(props) {
+//   return (
+//     <div>
+//       <h1>Index</h1>
+//       <p>{JSON.stringify(props.episodes)}</p>
+//     </div>
+//   )
+// }
+
+// export async function getServerSideProps() {
+//   const response = await fetch('http://localhost:3333/episodes');
+//   const data = await response.json();
+
+//   return {
+//     props: {
+//       episodes: data,
+//     }
+//   }
+// }
+
+//SPA
+// import { useEffect } from 'react';
+
+// export default function Home() {
+//   useEffect(() => {
+//     fetch('http://localhost:3333/episodes')
+//       .then(response => response.json())
+//       .then(data => console.log(data));
+//   }, []);
+
+//   return (
+//     <h1>Index</h1>
+//   )
+// }
+
+
+
