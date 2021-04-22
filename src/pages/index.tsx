47d6821182d41ba74364af2ddcd9c2b782639413
vivianebresolin <vivianebresolin@gmail.com>
2021-04-22 14:13:07 -0300

    Added dependency Axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import { api } from '../services/api';
 
 type Episode = {
   id: string,
@@ -22,8 +23,18 @@ export default function Home(props: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
+  //usando fetch
+  // const response = await fetch('http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=desc');
+  // const data = await response.json();
+
+  //usando axios
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    }
+  });
 
   return {
     props: {
