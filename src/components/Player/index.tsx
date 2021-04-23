5c53f1e582977d9e3c02d368f145f03ea32785f1
vivianebresolin <vivianebresolin@gmail.com>
2021-04-23 11:09:39 -0300

    Added 'PlayerContext'

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,6 +1,15 @@
+import { useContext } from 'react';
+import Image from 'next/image';
+import { PlayerContext } from '../../contexts/PlayerContext';
 import styles from './styles.module.scss';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 export function Player() {
+    const { episodeList, currentEpisodeIndex } = useContext(PlayerContext);
+
+    const episode = episodeList[currentEpisodeIndex];
+
     return (
         <div className={styles.playerContainer}>
             <header>
@@ -8,15 +17,36 @@ export function Player() {
                 <strong>Tocando agora</strong>
             </header>
 
-            <div className={styles.emptyPlayer}>
-                <strong>Selecione um podcast para ouvir</strong>
-            </div>
+            { episode ? (
+                <div className={styles.currentEpisode}>
+                    <Image
+                        width={592}
+                        height={592}
+                        src={episode.thumbnail}
+                        objectFit="cover"
+                    />
+                    <strong>{episode.title}</strong>
+                    <span>{episode.members}</span>
+                </div>
+            ) : (
+                <div className={styles.emptyPlayer}>
+                    <strong>Selecione um podcast para ouvir</strong>
+                </div>
+            )}
 
-            <footer className={styles.empty}>
+            <footer className={!episode ? styles.empty : ''}>
                 <div className={styles.progress}>
                     <span>00:00</span>
                     <div className={styles.slider}>
-                        <div className={styles.emptySlider} />
+                        {episode ? (
+                            <Slider
+                                trackStyle={{ backgroundColor: '#84d361' }}
+                                railStyle={{ backgroundColor: '#9f75ff' }}
+                                handleStyle={{ borderColor: '#84d361', borderWidth: 4 }}
+                            />
+                        ) : (
+                            <div className={styles.emptySlider} />
+                        )}
                     </div>
                     <span>00:00</span>
                 </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,29 @@ import '../styles/global.scss';
 
 import { Header } from "../components/Header";
 import { Player } from "../components/Player";
-
+import { PlayerContext } from '../contexts/PlayerContext';
 import styles from '../styles/app.module.scss';
-
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setcurrentEpisodeIndex] = useState(0);
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    setcurrentEpisodeIndex(0);
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   );
 }
 
